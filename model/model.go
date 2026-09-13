e3432a664170f545c2cf78d7ebee974c3a914929
Gaido <luisfernandogaido@gmail.com>
2026-09-13 17:21:16 -0300

    Simulação de venda direta não apresenta tabela REG em prazo de 36 meses

--- model/model.go
+++ model/model.go
@@ -5,15 +5,27 @@ import (
 	"crypto/tls"
 
 	"github.com/redis/go-redis/v9"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+const (
+	uri = "mongodb://gaido:xW3g3JAhsZjE-@72.60.250.80/storage?authSource=admin"
 )
 
 var (
 	RedisCore *redis.Client
 	RedisEdge *redis.Client
 	Ctx       context.Context
+	client    *mongo.Client
+	db        *mongo.Database
 )
 
 func init() {
+	var (
+		err error
+	)
+	db = client.Database("redistic")
 	RedisCore = redis.NewClient(&redis.Options{
 		Addr:     "72.60.250.80:6380",
 		Password: "sbCK7YLM0qcRM6c",
@@ -26,4 +38,15 @@ func init() {
 		Addr: "127.0.0.1:6379",
 	})
 	Ctx = context.Background()
+	client, err = mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	doc := struct {
+		Nome  string
+		Idade int
+	}{"Gaido", 44}
+	if _, err := db.Collection("dlq").InsertOne(nil, doc); err != nil {
+		panic(err)
+	}
 }
